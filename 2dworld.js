0da6e70ec69c69469bf9248f33e1c76df87b017e
dalianmao <dalianmao22233@users.noreply.github.com>
2016-09-13 17:30:43 -0700

    Update 2dworld.js

--- 2dworld.js
+++ 2dworld.js
@@ -46,11 +46,11 @@ ref_count_set.on('value', function (snapshot) {
 //                list.push(tmp);
 //            }
 //        console.log("list:" + list[0][0]);
-            var width = 400, height = 400;
+            var width = 500, height = 400;
             // var width = $("#container_2d").width();
             // var height = $("#container_2d").height();
             // var width = 400, height = 500;
-            console.log("width: " + width + ", height: " + height); 
+            // console.log("width: " + width + ", height: " + height); 
             var projection = d3.geo.albersUsa();//shiyuxin
             // Setting color domains(intervals of values) for our map
 
